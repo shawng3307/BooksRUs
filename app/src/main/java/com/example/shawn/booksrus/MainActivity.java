c94fd21cc26706c77ac635102329d79c5173de86
Shawn George <shawng3307@gmail.com>
2018-06-04 01:32:13 -0500

    Displays Text
    Once submitted, entered text is displayed in toast

--- app/src/main/java/com/example/shawn/booksrus/MainActivity.java
+++ app/src/main/java/com/example/shawn/booksrus/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.shawn.booksrus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button searchButton = (Button) findViewById(R.id.search);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText edit = (EditText) findViewById(R.id.text);
+                String result = edit.getText().toString();
+
+                Toast toast = Toast.makeText(MainActivity.this, result, Toast.LENGTH_LONG);
+                toast.show();
+
+                //clears Edit Text view
+                edit.setText("");
+            }
+        });
     }
+
+
 }
