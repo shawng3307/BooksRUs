13496d28166065a0a123d583dfaf896f26a4fc46
Shawn George <shawng3307@gmail.com>
2018-06-14 18:17:25 -0500

    Networking
    Connecting to google API

---
+++ app/src/main/java/com/example/shawn/booksrus/Event.java
@@ -0,0 +1,11 @@
+package com.example.shawn.booksrus;
+
+public class Event {
+    public final String title;
+    public final String author;
+
+    public Event(String bookTitle, String bookAuthor){
+        title = bookTitle;
+        author = bookAuthor;
+    }
+}
--- app/src/main/java/com/example/shawn/booksrus/MainActivity.java
+++ app/src/main/java/com/example/shawn/booksrus/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.shawn.booksrus;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+public class MainActivity extends AppCompatActivity {
+    public static final String REQUEST_URL =
+            "https://www.googleapis.com/books/v1/volumes?q=";
+
+    public String result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 EditText edit = (EditText) findViewById(R.id.text);
-                String result = edit.getText().toString();
+                result = edit.getText().toString();
 
                 Toast toast = Toast.makeText(MainActivity.this, result, Toast.LENGTH_LONG);
                 toast.show();
@@ -30,7 +39,51 @@ public class MainActivity extends AppCompatActivity {
                 edit.setText("");
             }
         });
+
+        BookAsyncTask task = new BookAsyncTask();
+        task.execute();
+
     }
 
+    private class BookAsyncTask extends AsyncTask<URL, Void, Event>{
+        @Override
+        protected Event doInBackground(URL... urls){
+            URL url = createURL(REQUEST_URL);
+
+            String jsonResponse = "";
+
+            try{
+                jsonResponse = makeHttpRequest(url);
+            }catch (IOException e){
+                Log.e("error", "problem in http request");
+            }
+
+            Event book = extractFeatJson(jsonResponse);
+
+            return book;
+        }
+
+        private URL createURL(String newURL){
+            URL url = null;
+            try{
+                result = result.replace(' ', '+');
+                String searched = newURL + result;
+                url = new URL(searched);
+            }catch(MalformedURLException ex){
+                Log.e("error", "creating url");
+                return null;
+            }
+
+            return url;
+        }
+
+        private String makeHttpRequest(URL url) throws IOException{
+            String jsonResponse = "";
+
+            if(url == null)
+                return jsonResponse;
+
+        }
+    }
 
 }
